fe69ec107b914ba60eb8eb18b00d16b9840cf7f5
Felix Boerner <ich@felix-boerner.de>
2017-08-25 07:45:32 +0200

    Download always new Makefile

--- init.sh
+++ init.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-if [ ! -f "Makefile" ]; then
-    wget -q https://raw.githubusercontent.com/escalate/ansible-makefile/master/Makefile
-fi
+wget -q https://raw.githubusercontent.com/escalate/ansible-makefile/master/Makefile -O Makefile
 
 if [ ! -f "pass.sh" ]; then
     echo -e "#!/bin/bash\ncat path/to/secret" > pass.sh
