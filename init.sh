c94d600a64d9a7d57791f6444eaa8f66100720b7
Felix Boerner <ich@felix-boerner.de>
2017-09-25 07:21:39 +0200

    feat: switch to original Makefile repository

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -q https://raw.githubusercontent.com/escalate/ansible-makefile/master/Makefile -O Makefile
+wget -q https://raw.githubusercontent.com/paulRbr/ansible-makefile/84230ccbd1521d2eea64f122f4a336f988a5d570/Makefile -O Makefile
 
 if [ ! -f "pass.sh" ]; then
     echo -e "#!/bin/bash\ncat path/to/secret" > pass.sh
