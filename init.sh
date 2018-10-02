a7a870ee79a1b670dccc4490102e6029eb7aedb9
Felix Boerner <ich@felix-boerner.de>
2018-10-02 08:59:16 +0200

    feat: remove basic ansible configuration

--- init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget -q https://raw.githubusercontent.com/paulRbr/ansible-makefile/84230ccbd1521d2eea64f122f4a336f988a5d570/Makefile -O Makefile
-
-if [ ! -f "pass.sh" ]; then
-    echo -e "#!/bin/bash\ncat path/to/secret" > pass.sh
-fi
