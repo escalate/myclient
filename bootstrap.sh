06f8e1a598a7fb373f8a8783d132915cb93c9c73
Felix Boerner <ich@felix-boerner.de>
2018-10-02 08:57:46 +0200

    feat: add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Install base packages"
+sudo apt-get install -y git make
+
+echo "Clone dotfiles repository"
+git clone https://github.com/escalate/dotfiles.git "${HOME}/dotfiles"
+
+echo "Clone myclient repository"
+git clone https://github.com/escalate/myclient.git "${HOME}/myclient"
+
+cd "${HOME}/myclient" && sudo make
