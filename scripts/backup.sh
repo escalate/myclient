3fd1bbd528da31ae62a2dca78a3574108211c99c
Felix Boerner <ich@felix-boerner.de>
2018-10-06 14:00:32 +0200

    feat: add backup script

---
+++ scripts/backup.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -e
+
+DATE=$(date +%Y_%m_%d)
+DIRS_TO_TAR=( "${HOME}/Dokumente" "${HOME}/Downloads" "${HOME}/Schreibtisch" "${HOME}/Bilder" "${HOME}/.local/share/Trash" "${HOME}/.purple" "${HOME}/secrets" "/etc" )
+DIRS_TO_RSYNC=( "${HOME}/VirtualBox VMs" )
+LOCAL_BACKUP_PATH="/tmp/backup_${DATE}"
+MOUNT_PATH="/mnt/nas"
+REMOTE_BACKUP_PATH="${MOUNT_PATH}/Backup"
+
+if [ ! -d "${LOCAL_BACKUP_PATH}" ]; then
+    mkdir -p "${LOCAL_BACKUP_PATH}"
+fi
+
+for d in "${DIRS_TO_TAR[@]}"; do
+    BACKUP_FILE="backup_$(basename ${d})_${DATE}.tar.bz2"
+    if [ ! -f "${LOCAL_BACKUP_PATH}/${BACKUP_FILE}" ]; then
+        echo "INFO: Backup directory ${d} to ${BACKUP_FILE}"
+        sudo tar -cjf "${LOCAL_BACKUP_PATH}/${BACKUP_FILE}" "${d}"
+        sudo chown ${USER}:${USER} "${LOCAL_BACKUP_PATH}/${BACKUP_FILE}"
+    else
+        echo "INFO: Backup ${BACKUP_FILE} already created"
+    fi
+done
+
+if grep -qs ${MOUNT_PATH} /proc/mounts; then
+    echo "INFO: Synology already mounted"
+else
+    ./mount-nas.sh
+fi
+
+if grep -qs ${MOUNT_PATH} /proc/mounts; then
+    echo "INFO: Transfer local backup ${LOCAL_BACKUP_PATH} to NAS ${REMOTE_BACKUP_PATH}"
+    sudo rsync -r -v --progress -z --size-only "${LOCAL_BACKUP_PATH}" "${REMOTE_BACKUP_PATH}"
+    DIR_BASE=$(basename $LOCAL_BACKUP_PATH)
+    echo "INFO: Check integrity of ${DIR_BASE} after transfer"
+    diff -rq "${LOCAL_BACKUP_PATH}" "${REMOTE_BACKUP_PATH}/${DIR_BASE}"
+
+    echo "INFO: Attention! Don't forget to delete local backups ${LOCAL_BACKUP_PATH} to save diskspace"
+
+    for d in "${DIRS_TO_RSYNC[@]}"; do
+        echo "INFO: Backup directory ${d} to ${REMOTE_BACKUP_PATH}"
+        sudo rsync -r -v --progress -z --size-only "${d}" "${REMOTE_BACKUP_PATH}"
+        DIR_BASE=$(basename "${d}")
+        echo "INFO: Check integrity of ${DIR_BASE} after transfer"
+        diff -rq "${d}" "${REMOTE_BACKUP_PATH}/${DIR_BASE}"
+    done
+fi
+
+./mount-nas.sh
---
+++ scripts/mount-nas.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+SMB_SERVER="$(cat ${HOME}/.smb/nas_server)"
+SMB_PATH="$(cat ${HOME}/.smb/nas_path)"
+SMB_CREDENTIAL_FILE="${HOME}/.smb/nas_credential"
+MOUNT_PATH="/mnt/nas"
+
+if [ $UID -eq 0 ]; then
+    echo "INFO: You are root! Everything fine..."
+    ROOT_CMD=""
+else
+    echo "INFO: You are not root! We use sudo to help you..."
+    ROOT_CMD="sudo"
+fi
+
+if [ ! -f "${SMB_CREDENTIAL_FILE}" ]; then
+    echo "ERROR: Login credentials not present!"
+    exit 1
+fi
+
+if [ ! -d ${MOUNT_PATH} ]; then
+    echo "INFO: Create local mount path ${MOUNT_PATH}"
+    ${ROOT_CMD} mkdir -p ${MOUNT_PATH}
+fi
+
+if grep -qs ${MOUNT_PATH} /proc/mounts; then
+    echo "INFO: Umount ${MOUNT_PATH} ..."
+    ${ROOT_CMD} umount ${MOUNT_PATH}
+else
+    echo "INFO: Mount //${SMB_SERVER}${SMB_PATH} to ${MOUNT_PATH} ..."
+    ${ROOT_CMD} mount -t cifs -o credentials=${SMB_CREDENTIAL_FILE} //${SMB_SERVER}${SMB_PATH} ${MOUNT_PATH}
+fi
