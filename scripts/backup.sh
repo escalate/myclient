24011d8cc6316d289032b4bf9b9ce9c811301806
Felix	Boerner <ich@felix-boerner.de>
2025-03-03 13:30:50 +0100

    style: format shell files

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,43 +2,43 @@
 set -e
 
 DATE=$(date +%Y_%m_%d)
-DIRS_TO_TAR=( "${HOME}/Desktop" "${HOME}/Documents" "${HOME}/Downloads" "${HOME}/Music" "${HOME}/Pictures" "${HOME}/.local/share" "${HOME}/.purple" "${HOME}/secrets" )
-DIRS_TO_RSYNC=( "${HOME}/VirtualBox VMs" )
+DIRS_TO_TAR=("${HOME}/Desktop" "${HOME}/Documents" "${HOME}/Downloads" "${HOME}/Music" "${HOME}/Pictures" "${HOME}/.local/share" "${HOME}/.purple" "${HOME}/secrets")
+DIRS_TO_RSYNC=("${HOME}/VirtualBox VMs")
 LOCAL_BACKUP_PATH="/tmp/backup_${DATE}"
 MOUNT_PATH="/mnt/nas"
 REMOTE_BACKUP_PATH="${MOUNT_PATH}/Backup"
 
 if [ ! -d "${LOCAL_BACKUP_PATH}" ]; then
-    mkdir -p "${LOCAL_BACKUP_PATH}"
+  mkdir -p "${LOCAL_BACKUP_PATH}"
 fi
 
 for d in "${DIRS_TO_TAR[@]}"; do
-    BACKUP_FILE="backup_$(basename "${d}")_${DATE}.tar"
-    if [ ! -f "${LOCAL_BACKUP_PATH}/${BACKUP_FILE}" ]; then
-        echo "INFO: Create backup archive \"${LOCAL_BACKUP_PATH}/${BACKUP_FILE}\" for directory \"${d}\""
-        sudo tar -cvf "${LOCAL_BACKUP_PATH}/${BACKUP_FILE}" "${d}"
-        sudo chown "${USER}":"${USER}" "${LOCAL_BACKUP_PATH}/${BACKUP_FILE}"
-    else
-        echo "INFO: Backup archive \"${LOCAL_BACKUP_PATH}/${BACKUP_FILE}\" for directory \"${d}\" already created"
-    fi
+  BACKUP_FILE="backup_$(basename "${d}")_${DATE}.tar"
+  if [ ! -f "${LOCAL_BACKUP_PATH}/${BACKUP_FILE}" ]; then
+    echo "INFO: Create backup archive \"${LOCAL_BACKUP_PATH}/${BACKUP_FILE}\" for directory \"${d}\""
+    sudo tar -cvf "${LOCAL_BACKUP_PATH}/${BACKUP_FILE}" "${d}"
+    sudo chown "${USER}":"${USER}" "${LOCAL_BACKUP_PATH}/${BACKUP_FILE}"
+  else
+    echo "INFO: Backup archive \"${LOCAL_BACKUP_PATH}/${BACKUP_FILE}\" for directory \"${d}\" already created"
+  fi
 done
 
 if grep -qs ${MOUNT_PATH} /proc/mounts; then
-    echo "INFO: NAS already mounted"
+  echo "INFO: NAS already mounted"
 else
-    ./mount-nas.sh
+  ./mount-nas.sh
 fi
 
 if grep -qs ${MOUNT_PATH} /proc/mounts; then
-    echo "INFO: Transfer local backup ${LOCAL_BACKUP_PATH} to NAS ${REMOTE_BACKUP_PATH}"
-    sudo rsync -r -v --progress -z --size-only "${LOCAL_BACKUP_PATH}" "${REMOTE_BACKUP_PATH}"
+  echo "INFO: Transfer local backup ${LOCAL_BACKUP_PATH} to NAS ${REMOTE_BACKUP_PATH}"
+  sudo rsync -r -v --progress -z --size-only "${LOCAL_BACKUP_PATH}" "${REMOTE_BACKUP_PATH}"
 
-    echo "INFO: Attention! Don't forget to delete local backups ${LOCAL_BACKUP_PATH} to save diskspace"
+  echo "INFO: Attention! Don't forget to delete local backups ${LOCAL_BACKUP_PATH} to save diskspace"
 
-    for d in "${DIRS_TO_RSYNC[@]}"; do
-        echo "INFO: Backup directory ${d} to ${REMOTE_BACKUP_PATH}"
-        sudo rsync -r -v --progress -z --size-only "${d}" "${REMOTE_BACKUP_PATH}"
-    done
+  for d in "${DIRS_TO_RSYNC[@]}"; do
+    echo "INFO: Backup directory ${d} to ${REMOTE_BACKUP_PATH}"
+    sudo rsync -r -v --progress -z --size-only "${d}" "${REMOTE_BACKUP_PATH}"
+  done
 fi
 
 ./mount-nas.sh
--- scripts/mount-nas.sh
+++ scripts/mount-nas.sh
@@ -7,27 +7,27 @@ SMB_CREDENTIAL_FILE="${HOME}/.smb/nas_credential"
 MOUNT_PATH="/mnt/nas"
 
 if [ $UID -eq 0 ]; then
-    echo "INFO: You are root! Everything fine..."
-    ROOT_CMD=""
+  echo "INFO: You are root! Everything fine..."
+  ROOT_CMD=""
 else
-    echo "INFO: You are not root! We use sudo to help you..."
-    ROOT_CMD="sudo"
+  echo "INFO: You are not root! We use sudo to help you..."
+  ROOT_CMD="sudo"
 fi
 
 if [ ! -f "${SMB_CREDENTIAL_FILE}" ]; then
-    echo "ERROR: Login credentials not present!"
-    exit 1
+  echo "ERROR: Login credentials not present!"
+  exit 1
 fi
 
 if [ ! -d ${MOUNT_PATH} ]; then
-    echo "INFO: Create local mount path ${MOUNT_PATH}"
-    ${ROOT_CMD} mkdir -p ${MOUNT_PATH}
+  echo "INFO: Create local mount path ${MOUNT_PATH}"
+  ${ROOT_CMD} mkdir -p ${MOUNT_PATH}
 fi
 
 if grep -qs ${MOUNT_PATH} /proc/mounts; then
-    echo "INFO: Umount ${MOUNT_PATH} ..."
-    ${ROOT_CMD} umount ${MOUNT_PATH}
+  echo "INFO: Umount ${MOUNT_PATH} ..."
+  ${ROOT_CMD} umount ${MOUNT_PATH}
 else
-    echo "INFO: Mount //${SMB_SERVER}${SMB_PATH} to ${MOUNT_PATH} ..."
-    ${ROOT_CMD} mount -t cifs -o credentials="${SMB_CREDENTIAL_FILE}" "//${SMB_SERVER}${SMB_PATH}" "${MOUNT_PATH}"
+  echo "INFO: Mount //${SMB_SERVER}${SMB_PATH} to ${MOUNT_PATH} ..."
+  ${ROOT_CMD} mount -t cifs -o credentials="${SMB_CREDENTIAL_FILE}" "//${SMB_SERVER}${SMB_PATH}" "${MOUNT_PATH}"
 fi
