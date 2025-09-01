5b8f9ee39367e6c28c0910247d3c93c45d6e0eb5
Felix Boerner <ich@felix-boerner.de>
2025-09-01 14:36:06 +0200

    feat: add python path"

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,6 +4,9 @@ set -e -E -u -C -o pipefail
 echo "Install base packages"
 sudo apt-get install --yes git make bzip2
 
+echo "Add Python path to PATH"
+echo "PATH=\"${HOME}/.local/bin:\${PATH}\"" >> "${HOME}/.bashrc"
+
 echo "Clone dotfiles repository"
 git clone https://github.com/escalate/dotfiles.git "${HOME}/dotfiles"
 
