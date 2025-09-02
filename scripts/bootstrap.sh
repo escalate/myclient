84f934e36dbcbc725713a5252bf502beac79c602
Felix Boerner <ich@felix-boerner.de>
2025-09-02 08:35:21 +0200

    feat: delete old ssh files

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -13,5 +13,8 @@ git clone https://github.com/escalate/dotfiles.git "${HOME}/dotfiles"
 echo "Clone myclient repository"
 git clone https://github.com/escalate/myclient.git "${HOME}/myclient"
 
+echo "Remove old .ssh files"
+rm --recursive --force "${HOME}/.ssh"
+
 cd "${HOME}/myclient" || exit
 make
