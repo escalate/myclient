971ab302abda6ca1b409e45b9d4d20c2fd236b44
Felix Boerner <ich@felix-boerner.de>
2025-09-02 09:46:37 +0200

    style: fix linter error

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,7 +5,7 @@ echo "Install base packages"
 sudo apt-get install --yes git make tar bzip2
 
 echo "Add Python path to PATH"
-echo "PATH=\"${HOME}/.local/bin:\${PATH}\"" >> "${HOME}/.bashrc"
+echo "PATH=\"${HOME}/.local/bin:\${PATH}\"" >>"${HOME}/.bashrc"
 
 echo "Clone dotfiles repository"
 git clone https://github.com/escalate/dotfiles.git "${HOME}/dotfiles"
